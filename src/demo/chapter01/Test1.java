8ea5738efec09f65e894140350bebcf926407f14
王涛 <desl0451@126.com>
2017-06-04 21:34:55 +0800

    打印图形
    aa

---
+++ src/demo/chapter01/Test1.java
@@ -0,0 +1,46 @@
+package demo.chapter01;
+
+//555555555
+//544444445
+//543333345
+//543222345
+//543212345
+//543222345
+//543333345
+//544444445
+//555555555
+
+public class Test1 {
+	public static void main(String[] args) {
+		for (int i = 1; i <= 9; i++) {
+			if (i <= 5) {
+				for (int j = 1; j <= i; j++) {
+					System.out.print(6 - j);
+				}
+				for (int k = 1; k < 11 - 2 * i - 1; k++) {
+					System.out.print(6 - i);
+				}
+				for (int n = 1; n <= i; n++) {
+					if (i == 5) {
+						if (n != 5) {
+							System.out.print(n + 1);
+						}
+					} else {
+						System.out.print(5 + n - i);
+					}
+				}
+			} else {
+				for (int j = 1; j <= 10 - i; j++) {
+					System.out.print(i - (i - 6) - j);
+				}
+				for (int k = 1; k <= (i - 5) * 2 - 1; k++) {
+					System.out.print(i - 4);
+				}
+				for (int n = 1; n <= 10 - i; n++) {
+					System.out.print(i - (5 - n));
+				}
+			}
+			System.out.println();
+		}
+	}
+}
---
+++ src/demo/chapter01/Test2.java
@@ -0,0 +1,18 @@
+package demo.chapter01;
+
+//5
+//54
+//543
+//5432
+//54321
+
+public class Test2 {
+	public static void main(String[] args) {
+		for (int i = 1; i <= 5; i++) {
+			for (int j = 1; j <= i; j++) {
+				System.out.print(6-j);
+			}
+			System.out.println();
+		}
+	}
+}
